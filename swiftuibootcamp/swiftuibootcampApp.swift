69557d76ba2112b3aa0f2d0b0052dbb320d628dc
018584-RD2(黃彥儒) <jimmy.huang@senao.com.tw>
2025-11-12 09:49:10 +0800

    Initial Commit

---
+++ swiftuibootcamp/swiftuibootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuibootcampApp.swift
+//  swiftuibootcamp
+//
+//  Created by YEN-JU HUANG on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct swiftuibootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
